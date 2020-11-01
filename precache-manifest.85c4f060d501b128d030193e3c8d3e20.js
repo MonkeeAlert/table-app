a912e77d275f31c36801b70b990c38643064c78e
Sergey <homevictim19@gmail.com>
2020-11-01 22:56:57 +0300

    Updates

---
+++ precache-manifest.85c4f060d501b128d030193e3c8d3e20.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84bbea31aa2cdff34ec4d3f4f87edcec",
+    "url": "/table-app/index.html"
+  },
+  {
+    "revision": "8f64381f398b0cfaae5d",
+    "url": "/table-app/static/css/main.06c42404.chunk.css"
+  },
+  {
+    "revision": "260ec86e13b867074ee2",
+    "url": "/table-app/static/js/2.a56fd4a8.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/table-app/static/js/2.a56fd4a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f64381f398b0cfaae5d",
+    "url": "/table-app/static/js/main.4427216f.chunk.js"
+  },
+  {
+    "revision": "fa0eee9f4efbc3b34d74",
+    "url": "/table-app/static/js/runtime-main.7d3c62d7.js"
+  },
+  {
+    "revision": "1d3a54bbb349052c6a7bd53bf96714b9",
+    "url": "/table-app/static/media/TrebuchetMS-Bold.1d3a54bb.ttf"
+  },
+  {
+    "revision": "ab3f7bbc1bfe2356bcf37f7e11f7a68f",
+    "url": "/table-app/static/media/TrebuchetMS-Bold.ab3f7bbc.woff"
+  },
+  {
+    "revision": "ac0aea2bc9d340ef39c1528796935b53",
+    "url": "/table-app/static/media/TrebuchetMS-Bold.ac0aea2b.woff2"
+  },
+  {
+    "revision": "ba3274de1c74b11ea0113bfa9be1af89",
+    "url": "/table-app/static/media/TrebuchetMS-Bold.ba3274de.eot"
+  },
+  {
+    "revision": "289d9923e53c7fa99fe4ea1a4415a9ae",
+    "url": "/table-app/static/media/TrebuchetMS.289d9923.ttf"
+  },
+  {
+    "revision": "af1d9a90239a3d6b8e107072521da874",
+    "url": "/table-app/static/media/TrebuchetMS.af1d9a90.woff2"
+  },
+  {
+    "revision": "ea21424b579b5cc4c3ad5af2c0977d90",
+    "url": "/table-app/static/media/TrebuchetMS.ea21424b.eot"
+  },
+  {
+    "revision": "f2bb6f2202822c63c05ffbcfb9cdbb54",
+    "url": "/table-app/static/media/TrebuchetMS.f2bb6f22.woff"
+  },
+  {
+    "revision": "2e617e171c93baff0608a4b07dc58f8e",
+    "url": "/table-app/static/media/ascending.2e617e17.svg"
+  },
+  {
+    "revision": "9abd6ee13de6478fd4ce8d33bbc7cd4a",
+    "url": "/table-app/static/media/descending.9abd6ee1.svg"
+  },
+  {
+    "revision": "0fb99c7634495abe5eae1a7ab5291887",
+    "url": "/table-app/static/media/done.0fb99c76.svg"
+  },
+  {
+    "revision": "d064fad2d1eaa54894611cf211ca22fb",
+    "url": "/table-app/static/media/forbidden.d064fad2.svg"
+  },
+  {
+    "revision": "956b42da2cf69d46eb47812787c44213",
+    "url": "/table-app/static/media/grid.956b42da.svg"
+  },
+  {
+    "revision": "aeec28632f4519bdb1c51013fead8ccd",
+    "url": "/table-app/static/media/not-done.aeec2863.svg"
+  },
+  {
+    "revision": "3274f879bdccaf63f2d9546424296731",
+    "url": "/table-app/static/media/table.3274f879.svg"
+  }
+]);
